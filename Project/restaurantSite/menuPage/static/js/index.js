a4317da4d2572c468a5ee03aecde835b86f699db
Andrew Dutton <56041430+814207579@users.noreply.github.com>
2024-04-09 20:48:02 -0700

    more js

--- Project/restaurantSite/menuPage/static/js/index.js
+++ Project/restaurantSite/menuPage/static/js/index.js
@@ -1,5 +1,7 @@
-let amongus = document.getElementById("amongus");
-amongus.onclick = function() {
+function changeAmongusSize() {
+   let amongus = document.getElementById("amongus");
    amongus.width = Math.random()*2000;
    amongus.height = Math.random()*2000;
 }
+
+setInterval(changeAmongusSize, 10)
