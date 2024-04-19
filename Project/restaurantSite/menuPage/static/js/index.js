6e0823575453901b50cf3e54b139cfe64bc31d4b
Andrew Dutton <56041430+814207579@users.noreply.github.com>
2024-04-18 19:54:38 -0700

    Added basic cart functionality
    - Added some javascript to make a basic cart function
    - Added temp link for the checkout page
    - Added temp buttons to menu page for adding to card

--- Project/restaurantSite/menuPage/static/js/index.js
+++ Project/restaurantSite/menuPage/static/js/index.js
@@ -1,7 +1,58 @@
+let cart = [];
+
 function changeAmongusSize() {
    let amongus = document.getElementById("amongus");
    amongus.width = Math.random();
    amongus.height = Math.random();
+}
+
+$(".AddToCartButton").each(function() {
+   $(this).click(addItemToCart);
+})
+
+function updateCart(item) {
+    let foodItems = $("#foodItems");
+    foodItems.append(item);
+}
+
+function addItemToCart() {
+    let foodItemValue = $(this).attr('value');
+    cart.push(foodItemValue);
+    console.log(foodItemValue);
+    updateCart(foodItemValue)
+}
+
+function logVal() {
+      console.log($(this).attr('value'));
+}
+
+
+//get the modal
+let modal = document.getElementById("myModal");
+
+//get the button
+let btn = document.getElementById("viewCartButton");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    if (cart.length > 0) {
+        modal.style.display = "block";
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 changeAmongusSize()
