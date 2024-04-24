34d06798fe8918ff5439f92312c3f1b3de2f1c6d
Andrew Dutton <56041430+814207579@users.noreply.github.com>
2024-04-23 20:11:38 -0700

    update

--- Project/restaurantSite/menuPage/admin.py
+++ Project/restaurantSite/menuPage/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-# from .models import Person
-#
-# admin.site.register(Person)
+from .models import Person
+admin.site.register(Person)
