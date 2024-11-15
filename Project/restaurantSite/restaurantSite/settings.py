5e2fec14ba317dbd4a6d52cf40cb9b4106a901b6
Andrew Dutton <56041430+814207579@users.noreply.github.com>
2024-11-14 23:28:25 -0800

    Changes to how images work for google cloud

--- Project/restaurantSite/restaurantSite/settings.py
+++ Project/restaurantSite/restaurantSite/settings.py
@@ -128,6 +128,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 APPEND_SLASH = True
 
+IMAGE_STORAGE = "https://storage.googleapis.com/sharktoothrestaurant.com/MainSite/menuPage/static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
